fb2f37b9c1c1398f666b11d14eafde8f04d422b4
thytran142 <thytran142@hotmail.com>
2023-03-12 16:58:48 -0400

    Javascript stopped at operators

---
+++ javascript/guess-the-number/script.js
@@ -0,0 +1,11 @@
+// an array variable name fruits,
+const fruits = ["apples", "bananas", "cherries"];
+// Cach thu 1
+for (const fruit of fruits) {
+    console.log(fruit);
+}
+// Cach thu 2
+console.log("Way 2====");
+for (let index = 0; index < fruits.length; index++) {
+    console.log(fruits[index]);
+}
---
+++ javascript/variable/README.md
@@ -0,0 +1,2 @@
+https://developer.mozilla.org/en-US/docs/Learn/JavaScript/First_steps/Test_your_skills:_variables
+
