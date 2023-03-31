3dd3ed29bb7b89660cbf07ae94064282b264967f
vthaihien1412 <v.thaihien1412@gmail.com>
2023-03-31 10:26:54 -0400

    exercise 10 done

---
+++ javascript/looping/guest.js
@@ -0,0 +1,18 @@
+const people = ['Chris', 'Anne', 'Colin', 'Terri', 'Phil', 'Lola', 'Sam', 'Kay', 'Bruce'];
+
+const admitted = document.querySelector('.admitted');
+const refused = document.querySelector('.refused');
+admitted.textContent = 'Admit: ';
+refused.textContent = 'Refuse: ';
+
+// loop starts here
+for (const person of people) {
+    if (person === 'Lola' || person === 'Phil' ) {
+        refused.textContent
+    } else {
+        admitted.textContent
+    }
+}
+refused.textContent = refused.textContent.slice(0,refused.textContent.length-2) + '.';
+admitted.textContent = admitted.textContent.slice(0,admitted.textContent.length-2) + '.';
+
